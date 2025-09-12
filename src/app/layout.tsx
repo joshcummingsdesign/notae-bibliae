6295c60af6247417b02cc5e910842b0af4b096e7
Josh Cummings <joshcummingsdesign@gmail.com>
2025-09-12 10:31:49 -0700

    add noindex, nofollow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
     initialScale: 1,
     width: "device-width",
   },
+  robots: "noindex, nofollow",
 };
 
 export default function RootLayout({
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function NotFound() {
+  return (
+    <main>
+      <Typography variant="h1">404 | Page Not Found</Typography>
+    </main>
+  );
+}
