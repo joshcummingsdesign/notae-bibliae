758574d805a890c1d6e13ce4547197ed6b24bbdd
Josh Cummings <joshcummingsdesign@gmail.com>
2025-09-17 16:30:40 -0700

    Fix link bug

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link, { LinkProps } from "next/link";
 import { useLoading } from "./LoadingProvider";
+import { usePathname } from "next/navigation";
 
 interface Props extends LinkProps {
   children: React.ReactNode;
@@ -8,10 +9,17 @@ interface Props extends LinkProps {
 
 export default function LinkWithTransition({ onClick, ...props }: Props) {
   const { setIsLoading } = useLoading();
+  const pathname = usePathname();
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     onClick && onClick(e);
+
+    // If pathname is same, bail
+    if (pathname === props.href.toString().split("?")[0]) return;
+
+    // If user is trying to open in a new tab, bail
     if (e.metaKey || e.ctrlKey || e.shiftKey || e.altKey) return;
+
     setIsLoading(true);
   };
 
