8e3bbceb162984c86823c50791c93468addd444f
Josh Cummings <joshcummingsdesign@gmail.com>
2025-12-17 21:13:11 -0800

    dialy office text cleanup

--- src/components/DailyOffice/OpeningVersicles.tsx
+++ src/components/DailyOffice/OpeningVersicles.tsx
@@ -16,34 +16,49 @@ export const OpeningVersicles: React.FC<Props> = ({ office }) => {
     );
   }
 
+  const header = (
+    <p>
+      [ <em>Stand</em> ]
+    </p>
+  );
+
   if (office === "evening-prayer") {
     if (calendar.isPreLent() || calendar.isLent()) {
       return (
-        <Image
-          src="/chants/evensong-opening-versicles.svg"
-          alt="Evening Prayer Opening Versicles"
-          width="600"
-          height="337"
-        />
+        <>
+          {header}
+          <Image
+            src="/chants/evensong-opening-versicles.svg"
+            alt="Evening Prayer Opening Versicles"
+            width="600"
+            height="337"
+          />
+        </>
       );
     } else {
       return (
-        <Image
-          src="/chants/evensong-opening-versicles-alleluia.svg"
-          alt="Evening Prayer Opening Versicles with Alleluia"
-          width="600"
-          height="337"
-        />
+        <>
+          {header}
+          <Image
+            src="/chants/evensong-opening-versicles-alleluia.svg"
+            alt="Evening Prayer Opening Versicles with Alleluia"
+            width="600"
+            height="337"
+          />
+        </>
       );
     }
   }
 
   return (
-    <Image
-      src="/chants/matins-opening-versicles.svg"
-      alt="Morning Prayer Opening Versicles"
-      width="600"
-      height="77"
-    />
+    <>
+      {header}
+      <Image
+        src="/chants/matins-opening-versicles.svg"
+        alt="Morning Prayer Opening Versicles"
+        width="600"
+        height="77"
+      />
+    </>
   );
 };
