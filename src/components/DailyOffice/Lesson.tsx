4bba4726345389ffe1db092e650d55c14bbfdc57
Josh Cummings <joshcummingsdesign@gmail.com>
2026-01-02 13:34:49 -0800

    update sunday readings

--- src/components/DailyOffice/Lesson.tsx
+++ src/components/DailyOffice/Lesson.tsx
@@ -11,16 +11,6 @@ export const Lesson: React.FC<Props> = ({ office, lesson }) => {
   const lessons = new Lessons(calendar);
   const lessonData = lessons.getToday();
 
-  // TODO: Find alternatives for deuterocanonical books
-  console.log(
-    Object.entries(lessons.getAll())
-      .slice(350)
-      .reduce<any>((acc, [k, v]) => {
-        acc[k] = v;
-        return acc;
-      }, {})
-  );
-
   return (
     <p>
       <strong>{lessonData[office][lesson]}</strong>
